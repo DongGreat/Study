18e2098cb443857b17f0b5fcd7194a6207a0e13a
DongGreat <sinzzanggu3@gmail.com>
2021-07-15 03:53:36 +0900

    Revert "코딩"
    This reverts commit 6c5fccd7fdd8e0c0e2ded67506d21d70615c85ff.

--- Python/파이썬 강좌 코딩 기초 강의 Python - 김왼손의 왼손코딩/#17 문자열 메서드.py
+++
@@ -1,9 +0,0 @@
-my_name = '김동현 김동현'
-
-print(my_name.split())
-
-fruit_str = '거봉 수박 포도 복숭아 망고 딸기 배 참외 찹쌀떡'
-
-print(fruit_str)
-print(fruit_str.split())
-
--- Python/파이썬 강좌 코딩 기초 강의 Python - 김왼손의 왼손코딩/#19 end, 이스케이프 코드.py
+++
@@ -1,8 +0,0 @@
-print('집단지성')
-print('집단지성', end='')
-print('집단지성', end='/')
-
-print('미운코딩새끼의\n\t집단\n지성들')
-
-print('미운', end='\t')
-print('코딩')

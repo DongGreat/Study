2291860211883c4bc96a4630cbc447634d7a6820
DongGreat <sinzzanggu3@gmail.com>
2021-07-19 02:02:30 +0900

    공부

---
+++ Python/딥러닝을 위한 파이썬 레벨1 - 런어데이/Exercise.1-20 Squared Error.py
@@ -0,0 +1,8 @@
+pred1, pred2, pred3 = 10, 20, 30
+y1, y2, y3 = 10, 25, 40
+
+s_error1 = (pred1 - y1)**2
+s_error2 = (pred2 - y2)**2
+s_error3 = (pred3 - y3)**2
+
+print(s_error1, s_error2, s_error3)

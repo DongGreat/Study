540c110e88d239f1f7498e7f4cb33a5d5490aadf
DESKTOP-TSMS90P\sinzz <sinzzanggu3@gmail.com>
2022-11-21 10:02:28 +0900

    공부

---
+++ 명품 C++ Programming/OpenChallenge/chap7/chap7.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Histogram {
+    string text;
+public:
+    Histogram(string s = "") { text = s; }
+    Histogram& operator<<(string s);
+    Histogram& operator<<(char c);
+    void operator!();
+};
+
+Histogram& Histogram::operator<<(string s) {
+    text += s;
+    return *this;
+}
+
+Histogram& Histogram::operator<<(char c) {
+    text += c;
+    return *this;
+}
+
+void Histogram::operator!() {
+    cout << text << endl;
+
+    int count = 0;
+    int cnt[26] = { 0, };
+    for (int i = 0; i < text.length(); i++) {
+        if (isalpha(text[i])) {
+            count++;
+            for (int j = 0; j < 26; j++) {
+                if (tolower(text[i]) - 97 == j) {
+                    cnt[j]++;
+                    break;
+                }
+            }
+        }
+    }
+    cout << endl << "총 알파벳 수 " << count << endl;
+
+
+    for (int i = 0; i < 26; i++) {
+        cout << (char)(i + 'a') << ':';
+        for (int j = 0; j < cnt[i]; j++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    Histogram song("Wise men say, \nonly fools rush in but I can't help, \n");
+    song << "falling" << " in love with you." << "- by ";
+    song << 'k' << 'i' << 't';
+    !song;
+}
